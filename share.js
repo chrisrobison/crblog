1b9cc5b0b4e9f7f9e3bbfdcac849a91deaa86e25
Christopher Robison <cdr@netoasis.net>
2023-10-22 13:06:52 -0700

    Rolled my own social share library since everything I found was bloated, outdated or just plain ugly.

---
+++ share.js
@@ -0,0 +1,138 @@
+(function() {
+    let app = {
+         ...window.app,
+        defaults: {
+            url: location.href,
+            title: document.title,
+            user_id: "@thechrisrobison"
+        },
+        socialsites: [
+            { "title": "LinkedIn", "link": "https://www.linkedin.com/sharing/share-offsite/?url={url}", "icon": "/crblog/assets/share-icons/linkedin.svg" },
+            { "title": "HackerNews", "link": "https://news.ycombinator.com/submitlink?u={url}&t={title}", "icon": "/crblog/assets/share-icons/hacker.news.svg" },
+            { "title": "Reddit", "link": "https://reddit.com/submit?url={url}&title={title}", "icon": "/crblog/assets/share-icons/reddit.svg" },
+            { "title": "Twitter", "link": "https://twitter.com/intent/tweet?url={url}&text={title}&via=@thechrisrobison&hashtags={hash_tags}", "icon": "/crblog/assets/share-icons/twitter.svg" },
+            { "title": "Facebook", "link": "https://www.facebook.com/sharer.php?u={url}", "icon": "/crblog/assets/share-icons/facebook.svg" },
+            { "title": "Email", "link": "mailto:someone@somewhere.com&subject={title}&body=Add body here", "icon": "/crblog/assets/share-icons/email.svg" }
+        ],
+        init() {
+        },
+        showlist() {
+            let el = app.buildlist(app.socialsites);
+            let sharehtml = app.createElement("div", "sharehtml", "sharehtml", 
+                `<h2 style="width:100%;border-bottom:2px solid #fff;">Share<a title="Close Share Dialog" style="cursor:pointer;float:right;position:relative;top:-8px;" onclick="return app.closeshare()"><i class="fa-solid fa-circle-xmark"></i></a></h2>`, 
+                {
+                    backgroundColor: "#222",
+                    color: "#eee",
+                    width: "27rem",
+                    height: "14rem",
+                    display: "flex",
+                    border: "1rem solid #222",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    justifyContent: "space-between",
+                    position: "absolute",
+                    top: "40%",
+                    left: "30%",
+                    borderRadius: "1rem"
+                }); 
+            sharehtml.appendChild(el);
+            
+            let shareurl = app.createElement("input", "shareurl", "", "", { backgroundColor:"#fff",color:"#000",fontSize: "24px", width: "23rem", height: "3rem" }, "text", location.href)
+            shareurl.disabled = true;
+            let copy = app.createElement("i", "", "fas fa-paste", "", { fontSize: "24px", textAlign: "right", width: "2rem" });
+            copy.className = "fas fa-paste";
+            copy.title = "Copy to Clipboard";
+            copy.style.cursor = "pointer";
+            copy.onclick = app.copy;
+            
+            let inputwrap = app.createElement("div", "", "", "", {}, "", "");
+            inputwrap.appendChild(shareurl);
+            inputwrap.appendChild(copy);
+            sharehtml.appendChild(inputwrap);
+
+            document.querySelector(".content-wrapper").appendChild(sharehtml);
+            console.log("Loaded.");
+        },
+        closeshare() {
+            let el = document.querySelector("#sharehtml");
+            el.style.display = "none";
+            el.parentElement.removeChild(el);
+            return false;
+        },
+        createElement(tag="div", id, className, html, styles, type="", value="") {
+            let el = document.createElement(tag);
+            if (id) el.id = id;
+            if (className) el.className = className;
+            if (html) el.innerHTML = html;
+            if (type) el.type = type;
+            if (value) el.value = value;
+
+            if (styles) {
+                let keys = Object.keys(styles);
+                for (const style of keys) {
+                    el.style[style] = styles[style];
+                }
+            }
+            return el;
+        },
+        copy(evt) {
+            let el = document.querySelector("#shareurl");
+            el.select();
+            el.setSelectionRange(0, 999999);    // for mobile
+            navigator.clipboard.writeText(el.value);
+            app.copyval = el.value;
+
+            el.value = "COPIED";
+            el.style.textAlign = "center";
+            el.style.backgroundColor = "#ffa";
+            setTimeout(function() { let el = document.querySelector("#shareurl"); el.value = app.copyval; el.style.textAlign = "left", el.style.backgroundColor="#fff"; }, 2000);
+        },
+        buildlist(data) {
+            let out = document.createElement("div");
+            out.className = "sharelist";
+            out.style.display = "flex";
+            out.style.flexDirection = "row";
+            out.style.width = "100%";
+            out.style.justifyContent = "space-around";
+
+            let linkout = { url: location.href, title: document.title };
+            let link;
+            for (let site of data) {
+                link = site.link.replace(/\{([^\}]+)\}/g, function(match, p1) {
+                    if (linkout[p1]) {
+                        return encodeURIComponent(linkout[p1]);
+                    }
+                });
+                out.innerHTML += `<a class="social-wrap" href="${link}" title="Share on ${site.title}" onclick="return app.share('${site.title}')"><img src="${site.icon}" class="social-icon"></a>`;
+            }
+
+            return out;
+        },
+        /**
+         * share(who, { title: "title", url: "url", user_id: "user_id", hash_tags: "hash_tags" })
+         * 
+         **/
+        share(who, obj) {
+            if (!obj) obj = {};
+            if (!obj.url) obj.url = location.href;
+            if (!obj.title) obj.title = document.title;
+
+            let socialsite = app.socialsites.find(function(obj) { return obj.title == who; });
+            if (socialsite) {
+               let link = socialsite.link.replace(/\{([^\}]+)\}/g, function(match, p1) {
+                   console.log(`Matched ${p1}`);
+                   console.dir(match);
+                   return encodeURIComponent(obj[p1]);
+                });
+                console.log(`Link: ${link}`);
+                window.open(link, "_blank");
+            }
+            return false;
+        },
+        state: { }
+
+    };
+    window.app = app;
+    app.init();
+    
+})();
