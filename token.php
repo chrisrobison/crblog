08f6df7851c2b7b9fe37adc93609c14fcfbd3182
Christopher Robison <cdr@netoasis.net>
2023-11-20 15:32:08 -0800

    Added new blog post

--- token.php
+++ token.php
@@ -1,7 +1,9 @@
 <?php
-$now = base64_encode(date("Ymdh"));
+$token = uniqid(); // base64_encode(date("Ymdh"));
 $out = new stdClass();
-$out->token = $now;
+$out->token = $token;
+
+file_put_contents(".tokens/{$token}", json_encode($_SERVER));
 
 header("Content-Type: application/json");
 print json_encode($out);
