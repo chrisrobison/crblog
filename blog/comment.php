0d2ec035200de94853f548e14bdb6267cf0d0c9e
Christopher Robison <cdr@netoasis.net>
2023-11-29 05:11:42 -0800

    Added extra safety check for comments to ensure no arbitrary html can be used in comments

--- blog/comment.php
+++ blog/comment.php
@@ -2,11 +2,22 @@
 session_start();
 $in = $_REQUEST;
 $out = array();
+$ref = $_SERVER['HTTP_REFERER'];
+$host = $_SERVER['HTTP_HOST'];
 
+if (preg_match("/^http[s]?:\/\/([^\/]*)(\/.*)/", $ref, $matches)) {
+    $refHost = $matches[1];
+    $refPath = $matches[2];
+    if ($refHost != $host) {
+        
+    }
+}
 if (array_key_exists("page", $in)) {
 
     if (array_key_exists('x', $in) && $in['x']==="save") {
         $json = file_get_contents("php://input");
+        $json = htmlspecialchars(strip_tags($json),ENT_NOQUOTES);
+
         if (file_exists("comments/{$in['page']}.json")) {
             $now = date("Ymdhis");
             $exe = `cp comments/{$in['page']}.json comments/.archive/{$in['page']}-$now.json`;
@@ -16,7 +27,7 @@ if (array_key_exists("page", $in)) {
     }
     
     if (file_exists("comments/{$in['page']}.json")) {
-        $out = json_decode(file_get_contents("comments/{$in['page']}.json"));
+        $out = json_decode(htmlspecialchars(strip_tags(file_get_contents("comments/{$in['page']}.json"))));
     }
 }
 header("Content-Type: application/json");
